c548eb748c42d48abd976e7b24de4340e8e841a6
Serhii Tarasenko <stdev33@gmail.com>
2024-03-07 14:35:18 +0200

    remove optional functionality of disabling the Filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  updateFilter,
-  selectFilter,
-  selectContacts,
-} from '../../redux/contactsSlice';
-import { isWhitespacesOrEmpty } from '../../helpers/helpers';
+import { updateFilter, selectFilter } from '../../redux/contactsSlice';
 import css from './Filter.module.css';
 
 function Filter() {
   const filter = useSelector(selectFilter);
-  const contacts = useSelector(selectContacts);
   const dispatch = useDispatch();
-
-  const isDisabled = contacts.length < 2 && isWhitespacesOrEmpty(filter);
 
   return (
     <div className={css.filterContainer}>
@@ -24,7 +16,6 @@ function Filter() {
           className={css.filterInput}
           value={filter}
           onChange={e => dispatch(updateFilter(e.target.value))}
-          disabled={isDisabled}
         />
       </label>
     </div>
